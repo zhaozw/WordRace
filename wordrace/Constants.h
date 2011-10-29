7b753f38b1d5402636d66e8138629d13a85942ce
Taha Selim Bebek <tsbebek@gmail.com>
2011-10-29 23:11:17 +0300

    adjustments for french

--- wordrace/Constants.h
+++ wordrace/Constants.h
@@ -10,8 +10,8 @@
 #define LANGUAGE [NSString stringWithFormat:@"turkish"]
 #define BUILDDATABASEMODE NO
 
-#define ALERTVIEW_CANCELBUTTON_TITLE @"TAMAM"
-#define ALERTVIEW_ERROR_TITLE @"HATA!"
+#define ALERTVIEW_CANCELBUTTON_TITLE @"OK"
+#define ALERTVIEW_ERROR_TITLE @"ERROR!"
 
 #define FACEBOOK_APPID @"107620642677158"
 #define FACEBOOK_APPSECRET @"7b4e98effc1afdafae4abd97a066d363"
@@ -22,44 +22,44 @@
 #define APP_LINK @"http://bit.ly/q2J2Pf"
 #define APP_PICTURE_LINK @"http://bebeksel.net/ingilizceky.jpg"
 
-#define TWITTERMESSAGE @"#ingilizcekelimeyarisi iPhone oyununda (%@) %i puan aldım ve %i. seviyeye çıktım. İndirmek için - %@"
-#define FACEBOOKMESSAGE @"İngilizce Kelime Yarışı iPhone oyununda (%@) %i puan aldım ve %i. seviyeye çıktım."
-#define FACEBOOKDESCRIPTION @"İngilizce Kelime Yarışı iPhone oyununu oynayarak hiç sıkılmadan yeni kelimeler öğreniyorum. Sizde indirmek için yukarıdaki linke tıklayabilirsiniz."
+#define TWITTERMESSAGE @"I've scored %i on #CourseAnglais (%@) and reached level %i. Download here - %@"
+#define FACEBOOKMESSAGE @"I've scored %i on #CourseAnglais (%@) and reached level %i. Download here - %@"
+#define FACEBOOKDESCRIPTION @""
 
-#define WELCOMEVC_GAMENAME @"KELİME YARIŞI"
-#define WELCOMEVC_PLAYBUTTON_TITLE @"OYNA"
-#define WELCOMEVC_LEVELBUTTON_TITLE @"SEVİYE"
-#define WELCOMEVC_SCORESBUTTON_TITLE @"SKORLAR"
+#define WELCOMEVC_GAMENAME @"Course Anglais"
+#define WELCOMEVC_PLAYBUTTON_TITLE @"Play"
+#define WELCOMEVC_LEVELBUTTON_TITLE @"Level"
+#define WELCOMEVC_SCORESBUTTON_TITLE @"Leaderboard"
 
-#define SELECTLEVEL_NAVIGATIONBAR_TITLE @"SEVİYE SEÇ"
-#define SELECTLEVEL_LEVEL_TITLE @"SEVİYE"
+#define SELECTLEVEL_NAVIGATIONBAR_TITLE @"Choose Level"
+#define SELECTLEVEL_LEVEL_TITLE @"Level"
 
-#define LEADERBOARD_NAVIGATIONBAR_TITLE @"SKORLAR"
+#define LEADERBOARD_NAVIGATIONBAR_TITLE @"Leaderboard"
 
-#define GAMEMODE_NAVIGATIONBAR_TITLE @"OYUN MODU"
-#define GAMEMODE_THREELIVES_TITLE @"ÜÇ CAN"
-#define GAMEMODE_VSTHECLOCK_TITLE @"ZAMANA KARŞI"
-#define GAMEMODE_SUDDENDEATH_TITLE @"ANİ ÖLÜM"
+#define GAMEMODE_NAVIGATIONBAR_TITLE @"Game Mode"
+#define GAMEMODE_THREELIVES_TITLE @"Three Lives"
+#define GAMEMODE_VSTHECLOCK_TITLE @"VS The Clock"
+#define GAMEMODE_SUDDENDEATH_TITLE @"Sudden Death"
 
-#define PAUSEVC_NAVIGATIONBAR_TITLE @"DURDUR"
-#define PAUSEVC_RESUME_TITLE @"DEVAM ET"
-#define PAUSEVC_RESTART_TITLE @"YENİDEN BAŞLA"
-#define PAUSEVC_GOTOMAINMENU_TITLE @"ANA MENÜYE GİT"
+#define PAUSEVC_NAVIGATIONBAR_TITLE @"Pause"
+#define PAUSEVC_RESUME_TITLE @"Resume"
+#define PAUSEVC_RESTART_TITLE @"Restart"
+#define PAUSEVC_GOTOMAINMENU_TITLE @"Main Menu"
 
-#define GAMEOVER_NAVIGATIONBAR_TITLE @"OYUN BİTTİ"
-#define GAMEOVER_NAMELABEL_TITLE @"İSİM"
-#define GAMEOVER_SCORELABEL_TITLE @"SKOR"
-#define GAMEOVER_HIGHSCORELABEL_TITLE @"REKOR"
-#define GAMEOVER_RESTARTLABEL_TITLE @"TEKRAR OYNA"
-#define GAMEOVER_SCORESLABEL_TITLE @"SKORLAR"
-#define GAMEOVER_GOTOMAINMENULABEL_TITLE @"ANA MENÜ"
-#define GAMEOVER_MOREGAMESLABEL_TITLE @"DİĞER OYUNLAR"
-#define GAMEOVER_SHARED_ON_FACEBOOK_ALERT_MESSAGE @"Facebook'da paylaşıldı"
-#define GAMEOVER_SHARED_ON_TWITTER_ALERT_MESSAGE @"Twitter'da paylaşıldı"
+#define GAMEOVER_NAVIGATIONBAR_TITLE @"Game Over"
+#define GAMEOVER_NAMELABEL_TITLE @"Name"
+#define GAMEOVER_SCORELABEL_TITLE @"Score"
+#define GAMEOVER_HIGHSCORELABEL_TITLE @"High Score"
+#define GAMEOVER_RESTARTLABEL_TITLE @"Restart"
+#define GAMEOVER_SCORESLABEL_TITLE @"Leaderboard"
+#define GAMEOVER_GOTOMAINMENULABEL_TITLE @"Main Menu"
+#define GAMEOVER_MOREGAMESLABEL_TITLE @"Other Games"
+#define GAMEOVER_SHARED_ON_FACEBOOK_ALERT_MESSAGE @"Shared on Facebook"
+#define GAMEOVER_SHARED_ON_TWITTER_ALERT_MESSAGE @"Shared on Twitter"
 
-#define LEADERBOARD_THREELIVES_ID @"threelives"
-#define LEADERBOARD_VSTHECLOCK_ID @"vstheclock"
-#define LEADERBOARD_SUDDENDEATH_ID @"suddendeath"
+#define LEADERBOARD_THREELIVES_ID @"threelivesenglishfrench"
+#define LEADERBOARD_VSTHECLOCK_ID @"vstheclockenglishfrench"
+#define LEADERBOARD_SUDDENDEATH_ID @"suddendeathenglishfrench"
 
 
 
