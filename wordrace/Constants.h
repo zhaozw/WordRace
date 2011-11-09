49352d475b78e5b2ebf259b97a7bf926f4611664
Taha Selim Bebek <tahaselimbebek@tsbsmacbookair.local>
2011-11-09 21:07:05 +0200

    changes for submission

--- wordrace/Constants.h
+++ wordrace/Constants.h
@@ -10,8 +10,8 @@
 #define LANGUAGE [NSString stringWithFormat:@"turkish"]
 #define BUILDDATABASEMODE NO
 
-#define ALERTVIEW_CANCELBUTTON_TITLE @"حسناً"
-#define ALERTVIEW_ERROR_TITLE @"خطأّ!"
+#define ALERTVIEW_CANCELBUTTON_TITLE @"OK"
+#define ALERTVIEW_ERROR_TITLE @"ERROR!"
 
 #define FACEBOOK_APPID @"107620642677158"
 #define FACEBOOK_APPSECRET @"7b4e98effc1afdafae4abd97a066d363"
@@ -22,45 +22,44 @@
 #define APP_LINK @"http://bit.ly/q2J2Pf"
 #define APP_PICTURE_LINK @"http://bebeksel.net/ingilizceky.jpg"
 
-#define TWITTERMESSAGE @"قمت بإحراز %i على #سباق الكلمات الإنجليزية (%@) ووصلت إلى المستوى %i حمل هن %@"
-#define FACEBOOKMESSAGE @"قمت بإحراز %i على #سباق الكلمات الإنجليزية (%@) ووصلت إلى المستوى %i حمل هن %@"
+#define TWITTERMESSAGE @"I've scored %i on #ifunarabic (%@) and reached level %i. Download here - %@"
+#define FACEBOOKMESSAGE @"I've scored %i on #ifunarabic (%@) and reached level %i. Download here - %@"
 #define FACEBOOKDESCRIPTION @""
 
-#define WELCOMEVC_GAMENAME @"سباق الكلمة الإنجليزية"
-#define WELCOMEVC_PLAYBUTTON_TITLE @"العب"
-#define WELCOMEVC_LEVELBUTTON_TITLE @"اختر المستوى"
-#define WELCOMEVC_SCORESBUTTON_TITLE @"المتصدرون"
+#define WELCOMEVC_GAMENAME @"iFun Arabic"
+#define WELCOMEVC_PLAYBUTTON_TITLE @"Play"
+#define WELCOMEVC_LEVELBUTTON_TITLE @"Level"
+#define WELCOMEVC_SCORESBUTTON_TITLE @"Leaderboard"
 
-#define SELECTLEVEL_NAVIGATIONBAR_TITLE @"اختر المستوى"
-#define SELECTLEVEL_LEVEL_TITLE @"مستوى"
+#define SELECTLEVEL_NAVIGATIONBAR_TITLE @"Choose Level"
+#define SELECTLEVEL_LEVEL_TITLE @"Level"
 
-#define LEADERBOARD_NAVIGATIONBAR_TITLE @"المتصدرون"
+#define LEADERBOARD_NAVIGATIONBAR_TITLE @"Leaderboard"
 
-#define GAMEMODE_NAVIGATIONBAR_TITLE @"وضعية اللعبة"
-#define GAMEMODE_THREELIVES_TITLE @"الثلاثة أرواح"
-#define GAMEMODE_VSTHECLOCK_TITLE @"ضد الساعة"
-#define GAMEMODE_SUDDENDEATH_TITLE @"الموت المفاجئ"
+#define GAMEMODE_NAVIGATIONBAR_TITLE @"Game Mode"
+#define GAMEMODE_THREELIVES_TITLE @"Three Lives"
+#define GAMEMODE_VSTHECLOCK_TITLE @"VS The Clock"
+#define GAMEMODE_SUDDENDEATH_TITLE @"Sudden Death"
 
-#define PAUSEVC_NAVIGATIONBAR_TITLE @"إيقاف مؤقت"
-#define PAUSEVC_RESUME_TITLE @"استئناف"
-#define PAUSEVC_RESTART_TITLE @"إعادة البدء"
-#define PAUSEVC_GOTOMAINMENU_TITLE @"القائمة الرئيسية"
+#define PAUSEVC_NAVIGATIONBAR_TITLE @"Pause"
+#define PAUSEVC_RESUME_TITLE @"Resume"
+#define PAUSEVC_RESTART_TITLE @"Restart"
+#define PAUSEVC_GOTOMAINMENU_TITLE @"Main Menu"
 
-#define GAMEOVER_NAVIGATIONBAR_TITLE @"انتهت اللعبة"
-#define GAMEOVER_NAMELABEL_TITLE @"الاسم"
-#define GAMEOVER_SCORELABEL_TITLE @"النقاط"
-#define GAMEOVER_HIGHSCORELABEL_TITLE @"نقاط عالية"
-#define GAMEOVER_RESTARTLABEL_TITLE @"إعادة البدء"
-#define GAMEOVER_SCORESLABEL_TITLE @"المتصدرون"
-#define GAMEOVER_GOTOMAINMENULABEL_TITLE @"القائمة الرئيسية"
-#define GAMEOVER_MOREGAMESLABEL_TITLE @"ألعاب أخرى"
-#define GAMEOVER_SHARED_ON_FACEBOOK_ALERT_MESSAGE @"تمت مشاركته على الفيسبوك"
-#define GAMEOVER_SHARED_ON_TWITTER_ALERT_MESSAGE @"تمت مشاركته على تويتر"
+#define GAMEOVER_NAVIGATIONBAR_TITLE @"Game Over"
+#define GAMEOVER_NAMELABEL_TITLE @"Name"
+#define GAMEOVER_SCORELABEL_TITLE @"Score"
+#define GAMEOVER_HIGHSCORELABEL_TITLE @"High Score"
+#define GAMEOVER_RESTARTLABEL_TITLE @"Restart"
+#define GAMEOVER_SCORESLABEL_TITLE @"Leaderboard"
+#define GAMEOVER_GOTOMAINMENULABEL_TITLE @"Main Menu"
+#define GAMEOVER_MOREGAMESLABEL_TITLE @"Other Games"
+#define GAMEOVER_SHARED_ON_FACEBOOK_ALERT_MESSAGE @"Shared on Facebook"
+#define GAMEOVER_SHARED_ON_TWITTER_ALERT_MESSAGE @"Shared on Twitter"
 
-#define LEADERBOARD_THREELIVES_ID @"wordraceforarabsthreelives"
-#define LEADERBOARD_VSTHECLOCK_ID @"wordraceforarabsvstheclock"
-#define LEADERBOARD_SUDDENDEATH_ID @"wordraceforarabssuddendeath"
-
+#define LEADERBOARD_THREELIVES_ID @"ifunarabicthreelives"
+#define LEADERBOARD_VSTHECLOCK_ID @"ifunarabicvstheclock"
+#define LEADERBOARD_SUDDENDEATH_ID @"ifunarabicsuddendeath"
 
 
 
